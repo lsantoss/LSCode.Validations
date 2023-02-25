5299515f52ff57bad9addaa5d4788f551d104a82
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2023-02-25 17:39:13 -0300

    IsEmptyExtension - Classe e testes

---
+++ test/LSCode.Validations.Test.Unit/BooleanValidations/Extensions/IsEmptyExtensionTest.cs
@@ -0,0 +1,33 @@
+﻿using LSCode.Validations.BooleanValidations.Extensions;
+using NUnit.Framework;
+
+namespace LSCode.Validations.Test.Unit.BooleanValidations.Extensions
+{
+    internal class IsEmptyExtensionTest
+    {
+        [Test]
+        [TestCase("")]
+        public void IsEmpty_string_True_Success(string value)
+        {
+            //Act
+            var result = value.IsEmpty();
+
+            //Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [TestCase("aaa")]
+        [TestCase("aaa aaa")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void IsEmpty_sbyte_False_Success(string value)
+        {
+            //Act
+            var result = value.IsEmpty();
+
+            //Assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
