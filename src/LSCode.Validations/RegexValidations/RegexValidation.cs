c64a3095427fa818e9a3341e7ba6a509e24ea94b
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2023-02-25 19:48:38 -0300

    Documentando RegexValidation

--- src/LSCode.Validations/RegexValidations/RegexValidation.cs
+++ src/LSCode.Validations/RegexValidations/RegexValidation.cs
@@ -2,8 +2,14 @@
 
 namespace LSCode.Validations.RegexValidations
 {
+    /// <summary>Provides methods to facilitate string validation using Regex.</summary>
     public static class RegexValidation
     {
-        public static bool IsMatch(string value, string regex) => !string.IsNullOrWhiteSpace(value) && Regex.IsMatch(value, regex);
+        /// <summary>Indicates whether the specified regular expression finds a match in the specified input string.</summary>
+        /// <remarks>Already has null, empty, or whitespace string validation.</remarks>
+        /// <param name="value">The string to search for a match.</param>
+        /// <param name="pattern">The regular expression pattern to match.</param>
+        /// <returns>True if the regular expression finds a match. Otherwise, false.</returns>
+        public static bool IsMatch(string value, string pattern) => !string.IsNullOrWhiteSpace(value) && Regex.IsMatch(value, pattern);
     }
 }
