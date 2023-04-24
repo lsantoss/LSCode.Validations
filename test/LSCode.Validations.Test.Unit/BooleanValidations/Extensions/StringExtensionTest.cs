165be74705ffecf65a4c985e7d30144084ae7c37
Lucas Santos <39504808+lsantoss@users.noreply.github.com>
2023-04-24 09:38:32 -0300

    HasLengthGreaterOrEqualThanExtension - métodos e testes

--- test/LSCode.Validations.Test.Unit/BooleanValidations/Extensions/StringExtensionTest.cs
+++ test/LSCode.Validations.Test.Unit/BooleanValidations/Extensions/StringExtensionTest.cs
@@ -365,33 +365,6 @@ namespace LSCode.Validations.Test.Unit.BooleanValidations.Extensions
         }
 
         [Test]
-        [TestCase("Text cannot be null or empty", 27)]
-        [TestCase("Text cannot be null or empty", 28)]
-        [TestCase("tests", 4)]
-        [TestCase("tests", 5)]
-        public void HasGreaterOrEqualLengthThan_ShouldReturnTrue(string value, int length)
-        {
-            //Act
-            var result = value.HasGreaterOrEqualLengthThan(length);
-
-            //Assert
-            Assert.That(result, Is.True);
-        }
-
-        [Test]
-        [TestCase("Text cannot be null or empty", 29)]
-        [TestCase("tests", 6)]
-        [TestCase(null, 10)]
-        public void HasGreaterOrEqualLengthThan_ShouldReturnFalse(string value, int length)
-        {
-            //Act
-            var result = value.HasGreaterOrEqualLengthThan(length);
-
-            //Assert
-            Assert.That(result, Is.False);
-        }
-
-        [Test]
         [TestCase("Text cannot be null or empty", 29)]
         [TestCase("tests", 6)]
         public void HasSmallerLengthThan_ShouldReturnTrue(string value, int length)
